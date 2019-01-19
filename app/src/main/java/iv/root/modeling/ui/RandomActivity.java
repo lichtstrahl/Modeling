425de80a106f7c38bd264df86d5fd5149384497f
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-01-19 16:14:41 +0300

    Добавил возможность перезагрузки данных

--- app/src/main/java/iv/root/modeling/ui/RandomActivity.java
+++ app/src/main/java/iv/root/modeling/ui/RandomActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -43,6 +45,18 @@ public class RandomActivity extends AppCompatActivity {
     EditText input;
     @BindView(R.id.labelInput)
     TextView labelInput;
+    @BindView(R.id.label_low)
+    TextView labelLow;
+    @BindView(R.id.label_middle)
+    TextView labelMiddle;
+    @BindView(R.id.label_high)
+    TextView labelHigh;
+    @BindView(R.id.label_lowGenerator)
+    TextView labelLowGenerator;
+    @BindView(R.id.label_middleGenerator)
+    TextView labelMiddleGenerator;
+    @BindView(R.id.label_highGenerator)
+    TextView labelHighGenerator;
 
     private ListenerEditText listenerInput;
 
@@ -71,8 +85,20 @@ public class RandomActivity extends AppCompatActivity {
         double pr1 = Evaluation.evaluation(adapterLow.getList());
         double pr2 = Evaluation.evaluation(adapterMiddle.getList());
         double pr3 = Evaluation.evaluation(adapterHigh.getList());
-        String msg = String.format(Locale.ENGLISH, "%8.3f %8.3f %8.3f", pr1, pr2, pr3);
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        labelLow.setText(String.format(Locale.ENGLISH, "%8.3f", pr1));
+        labelMiddle.setText(String.format(Locale.ENGLISH, "%8.3f", pr2));
+        labelHigh.setText(String.format(Locale.ENGLISH, "%8.3f", pr3));
+    }
+
+    @OnClick(R.id.buttonEvaluationGenerator)
+    public void clickEvaluationGenerator() {
+        double pr1 = Evaluation.evaluation(adapterLowGenerator.getList());
+        double pr2 = Evaluation.evaluation(adapterMiddleGenerator.getList());
+        double pr3 = Evaluation.evaluation(adapterHighGenerator.getList());
+
+        labelLowGenerator.setText(String.format(Locale.ENGLISH, "%8.3f", pr1));
+        labelMiddleGenerator.setText(String.format(Locale.ENGLISH, "%8.3f", pr2));
+        labelHighGenerator.setText(String.format(Locale.ENGLISH, "%8.3f", pr3));
     }
 
 
@@ -84,88 +110,6 @@ public class RandomActivity extends AppCompatActivity {
 
         listenerInput = new ListenerEditText(input);
         listsObserver = new ListsResponseObserver(this::bindRandomLists, this::error);
-    }
-
-    private void bindRandomLists(ContainerForList container) {
-        bindLow(container.listLow);
-        bindMiddle(container.listMiddle);
-        bindHigh(container.listHigh);
-        progressLoading.setVisibility(View.GONE);
-    }
-
-    private void bindLow(List<Integer> list) {
-        adapterLow = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
-        listLow = findViewById(R.id.list_low);
-        listLow.setAdapter(adapterLow);
-        listLow.setLayoutManager(new LinearLayoutManager(this));
-
-        adapterLowGenerator = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
-        listLowGenerator = findViewById(R.id.list_lowGenerator);
-        listLowGenerator.setAdapter(adapterLowGenerator);
-        listLowGenerator.setLayoutManager(new LinearLayoutManager(this));
-
-        RandomGenerator.rand(0);
-        for (Integer value : list) {
-            adapterLow.append(value % 10);
-            double r = Math.abs(RandomGenerator.rand(2)*1000);
-            adapterLowGenerator.append((int)Math.round(r)%10);
-        }
-    }
-
-    private void bindMiddle(List<Integer> list) {
-        adapterMiddle = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
-        listMiddle = findViewById(R.id.list_middle);
-        listMiddle.setAdapter(adapterMiddle);
-        listMiddle.setLayoutManager(new LinearLayoutManager(this));
-
-
-        adapterMiddleGenerator = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
-        listMiddleGenerator = findViewById(R.id.list_middleGenerator);
-        listMiddleGenerator.setAdapter(adapterMiddleGenerator);
-        listMiddleGenerator.setLayoutManager(new LinearLayoutManager(this));
-
-        RandomGenerator.rand(0);
-        for (Integer value : list) {
-            adapterMiddle.append(10 + value % 90);
-            double r = Math.abs(RandomGenerator.rand(2)*1000);
-            adapterMiddleGenerator.append((int)(10 + Math.round(r) % 90));
-        }
-    }
-
-    private void bindHigh(List<Integer> list) {
-        adapterHigh = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
-        listHigh = findViewById(R.id.list_high);
-        listHigh.setAdapter(adapterHigh);
-        listHigh.setLayoutManager(new LinearLayoutManager(this));
-
-        adapterHighGenerator = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
-        listHighGenerator = findViewById(R.id.list_highGenerator);
-        listHighGenerator.setAdapter(adapterHighGenerator);
-        listHighGenerator.setLayoutManager(new LinearLayoutManager(this));
-
-        RandomGenerator.rand(0);
-        for (Integer value : list) {
-            adapterHigh.append(100 + value % 900);
-            double r = Math.abs(RandomGenerator.rand(2)*1000);
-            adapterHighGenerator.append((int)(100 + Math.round(r) % 900));
-        }
-    }
-
-    private void initAustralianRandom() {
-        progressLoading.setVisibility(View.VISIBLE);
-        Single<ResponseQRNS> resLow = App.requestRandomInteger(COUNT_VALUE);
-        Single<ResponseQRNS> resMiddle = App.requestRandomInteger(COUNT_VALUE);
-        Single<ResponseQRNS> resHigh = App.requestRandomInteger(COUNT_VALUE);
-
-        Observable.zip(resLow.toObservable(), resMiddle.toObservable(), resHigh.toObservable(), this::initLists)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(listsObserver);
-
-    }
-
-    private ContainerForList initLists(ResponseQRNS r1, ResponseQRNS r2, ResponseQRNS r3) {
-        return new ContainerForList(r1.getData(), r2.getData(), r3.getData());
     }
 
     @Override
@@ -189,7 +133,7 @@ public class RandomActivity extends AppCompatActivity {
                 }
             }
         });
-        initAustralianRandom();
+        initRandomValues();
     }
 
     @Override
@@ -197,6 +141,114 @@ public class RandomActivity extends AppCompatActivity {
         super.onStop();
         listsObserver.unsubscribe();
         listenerInput.unsubscribe();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_random, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItemUpdate:
+                initRandomValues();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void bindRandomLists(ContainerForList container) {
+        bindLow(container.listLow);
+        bindMiddle(container.listMiddle);
+        bindHigh(container.listHigh);
+        progressLoading.setVisibility(View.GONE);
+    }
+
+    private void bindLow(List<Integer> list) {
+        labelLow.setText(R.string.label_low);
+        labelLowGenerator.setText(R.string.label_low);
+
+        adapterLow = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
+        listLow = findViewById(R.id.list_low);
+        listLow.setAdapter(adapterLow);
+        listLow.setLayoutManager(new LinearLayoutManager(this));
+
+        adapterLowGenerator = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
+        listLowGenerator = findViewById(R.id.list_lowGenerator);
+        listLowGenerator.setAdapter(adapterLowGenerator);
+        listLowGenerator.setLayoutManager(new LinearLayoutManager(this));
+
+        RandomGenerator.rand(0);
+        for (Integer value : list) {
+            adapterLow.append(value % 10);
+            double r = Math.abs(RandomGenerator.rand(2)*1000);
+            adapterLowGenerator.append((int)Math.round(r)%10);
+        }
+    }
+
+    private void bindMiddle(List<Integer> list) {
+        labelMiddle.setText(R.string.label_middle);
+        labelMiddleGenerator.setText(R.string.label_middle);
+
+        adapterMiddle = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
+        listMiddle = findViewById(R.id.list_middle);
+        listMiddle.setAdapter(adapterMiddle);
+        listMiddle.setLayoutManager(new LinearLayoutManager(this));
+
+
+        adapterMiddleGenerator = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
+        listMiddleGenerator = findViewById(R.id.list_middleGenerator);
+        listMiddleGenerator.setAdapter(adapterMiddleGenerator);
+        listMiddleGenerator.setLayoutManager(new LinearLayoutManager(this));
+
+        RandomGenerator.rand(0);
+        for (Integer value : list) {
+            adapterMiddle.append(10 + value % 90);
+            double r = Math.abs(RandomGenerator.rand(2)*1000);
+            adapterMiddleGenerator.append((int)(10 + Math.round(r) % 90));
+        }
+    }
+
+    private void bindHigh(List<Integer> list) {
+        labelHigh.setText(R.string.label_high);
+        labelHighGenerator.setText(R.string.label_high);
+
+        adapterHigh = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
+        listHigh = findViewById(R.id.list_high);
+        listHigh.setAdapter(adapterHigh);
+        listHigh.setLayoutManager(new LinearLayoutManager(this));
+
+        adapterHighGenerator = new IntegerAdapter(new LinkedList<>(), LayoutInflater.from(this));
+        listHighGenerator = findViewById(R.id.list_highGenerator);
+        listHighGenerator.setAdapter(adapterHighGenerator);
+        listHighGenerator.setLayoutManager(new LinearLayoutManager(this));
+
+        RandomGenerator.rand(0);
+        for (Integer value : list) {
+            adapterHigh.append(100 + value % 900);
+            double r = Math.abs(RandomGenerator.rand(2)*1000);
+            adapterHighGenerator.append((int)(100 + Math.round(r) % 900));
+        }
+    }
+
+    private void initRandomValues() {
+        progressLoading.setVisibility(View.VISIBLE);
+        Single<ResponseQRNS> resLow = App.requestRandomInteger(COUNT_VALUE);
+        Single<ResponseQRNS> resMiddle = App.requestRandomInteger(COUNT_VALUE);
+        Single<ResponseQRNS> resHigh = App.requestRandomInteger(COUNT_VALUE);
+
+        Observable.zip(resLow.toObservable(), resMiddle.toObservable(), resHigh.toObservable(), this::initLists)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(listsObserver);
+
+    }
+
+    private ContainerForList initLists(ResponseQRNS r1, ResponseQRNS r2, ResponseQRNS r3) {
+        return new ContainerForList(r1.getData(), r2.getData(), r3.getData());
     }
 
     public void error(Throwable t) {
