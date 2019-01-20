4fe34a5ca911180138e309c48474720d5f451674
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-01-20 13:49:50 +0300

    Добавил кнопку "запуск" моделирования

--- app/src/main/java/iv/root/modeling/ui/activity/QueueActivity.java
+++ app/src/main/java/iv/root/modeling/ui/activity/QueueActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,5 +36,16 @@ public class QueueActivity extends AppCompatActivity {
     public static void start(Activity activity) {
         Intent intent = new Intent(activity, QueueActivity.class);
         activity.startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_modeling, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return false;
     }
 }
--- app/src/main/java/iv/root/modeling/ui/fragment/DTFragment.java
+++ app/src/main/java/iv/root/modeling/ui/fragment/DTFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import iv.root.modeling.R;
 
@@ -15,6 +18,7 @@ public class DTFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_modeling_dt, container, false);
+        setHasOptionsMenu(true);
         return view;
     }
 
@@ -26,5 +30,21 @@ public class DTFragment extends BaseFragment {
     @Override
     public String getName() {
         return NAME;
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItemRunModeling:
+                Toast.makeText(getActivity(), "DT run modeling", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
